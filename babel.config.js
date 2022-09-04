c72100fd46c6cfd36255cb9bf01a75b7a7b791a8
mafof <pavel.bykov@mafof.ru>
2022-09-04 19:50:48 +0300

    Добавил алиасы

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,8 @@ module.exports = function(api) {
             helpers: "./src/Helpers/index",
             hooks: "./src/Hooks/index",
             api: "./src/Services/API/index",
-            migration: "./src/Services/DB/Migrations/index"
+            migration: "./src/Services/DB/Migrations/index",
+            db: "./src/Services/DB/DB/index"
           }
         }
       ]
