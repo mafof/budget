9520b54e9d1b1aa5aa819da8541fc047b35d2947
mafof <pavel.bykov@mafof.ru>
2022-12-13 21:23:22 +0300

    Добавил дополнительное поле в таблице cost_product

--- src/Services/DB/Models/CostProductList.ts
+++ src/Services/DB/Models/CostProductList.ts
@@ -4,6 +4,7 @@ import BaseMigration from "./BaseModel";
  * Модель реализующая структуры таблицы cost_product, содержащая ценники на продукты с временем момента их ввода
  * @description Стурктура таблицы =>
  * product_id - ID продукта
+ * opertation_id - ID операции
  * money - Кол-во рублей/долларов/евро/... (стоимость в данной операции)
  * penny - Кол-во копеек/центов/евро цент/... (стоимость в данной операции)
  * currency - валюта (по умолчанию RUB)
@@ -19,13 +20,15 @@ class CostProduct extends BaseMigration {
     await this.sqlQuery(`
       CREATE TABLE IF NOT EXISTS ? (
         product_id STRING NOT NULL,
+        opertation_id integer NOT NULL,
         money integer NOT NULL DEFAULT 0,
         penny integer NOT NULL DEFAULT 0,
         currency string NOT NULL DEFAULT 'RUB',
         time_at integer NOT NULL,
         created_at integer NOT NULL,
         updated_at integer NOT NULL,
-        FOREIGN KEY(product_id) REFERENCES product_list(name)
+        FOREIGN KEY(product_id) REFERENCES product_list(name),
+        FOREIGN KEY(opertation_id) REFERENCES operation_list(id)
       )
     `, [this.tableName]);
 
