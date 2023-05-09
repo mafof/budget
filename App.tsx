b2de7a3eecb4dbb69e1397e20a57ab9eb64621da
mafof <pavel.bykov@mafof.ru>
2023-05-09 22:04:48 +0300

    Добавил библиотеку иконок

--- App.tsx
+++ App.tsx
@@ -1,31 +1,85 @@
+import React, { FC, ReactElement } from 'react'
+import { OpaqueColorValue } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import React from 'react'
+import { BottomTabNavigationOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { HomePage } from '@pages';
-import { Button } from 'react-native';
 
-export default function App() {
-  const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
+
+interface IBaseSettingIcon {
+  focused: boolean,
+  color: string | OpaqueColorValue | undefined,
+  size: number | undefined
+}
+
+interface ISettingIcon extends IBaseSettingIcon {
+  route: any
+}
+
+const App: FC = () => {
+  const getIcon = ({ route, focused, color, size }: ISettingIcon): ReactElement<Icon> => {
+    let iconName: any;
+    route = route.route;
+
+    switch(route.name) {
+      case 'Главная':
+        iconName = 'home'
+      break;
+      case 'Операции':
+        iconName = focused
+        ? 'md-list'
+        : 'md-list-outline'
+      break;
+      case 'Статистика':
+        iconName = focused
+        ? 'md-stats-chart'
+        : 'md-stats-chart-outline'
+      break;
+      case 'Настройки':
+        iconName = focused
+        ? 'md-settings'
+        : 'md-settings-outline'
+      break;
+    }
+
+    return <Icon name={iconName} size={size} color={color} />
+  }
+
+  const options: BottomTabNavigationOptions = {
+    headerLeft: () => (
+      <Icon
+        name="account-circle"
+        size={30}
+        style={{ marginLeft: 10 }}
+        onPress={() => alert('Здесь будет функционал смены аккаунтов')}
+      />
+    ),
+
+    headerRight: () => (
+      <Icon
+        name="search"
+        size={30}
+        style={{ marginRight: 10 }}
+        onPress={() => alert('Здесь будет функционал глобального поиска')}
+      />
+    )
+  }
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={(route: any) => ({ tabBarIcon: (baseSettingIcon: IBaseSettingIcon) => getIcon({route, ...baseSettingIcon}) })}
+      >
         <Tab.Screen 
-          name="Main"
+          name="Главная"
           component={HomePage}
-          options={{
-            headerTitleAlign: 'center',
-            headerTitle: 'Главная',
-            headerRight: () => (
-              <Button
-                onPress={() => alert('This is a button!')}
-                title="Поиск"
-              />
-            )
-          }} 
+          options={options} 
         />
       </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
+
+export default App
