17ee832d4a6e42d18b45fc0116a753084d953ba5
mafof <pavel.bykov@mafof.ru>
2023-05-12 00:36:27 +0300

    Изменил конфигурацию и добавил зависимости

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,19 @@
 module.exports = api => {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
       [
         'module-resolver',
         {
-          root: ['./src'],
           extensions: [
             '.ts',
-            '.tsx',
-            '.jsx',
-            '.js',
-            '.json',
-            '.svg',
-            '.jpg',
-            '.png',
+            '.tsx'
           ],
           alias: {
             "@pages": "./src/Pages/index.ts",
-            "@stack": "./src/Pages/Stack/index.ts",
-            // "@helpers": "./src/Helpers/index.ts",
             "@hooks": "./src/Hooks/index.ts",
-            // "@api": "./src/Services/API/index.ts",
-            // "@db": "./src/Services/DB/index.ts",
-            // "@entities": "./src/Services/DB/Entities/index.ts"
+            "@components": "./src/Components/index.ts"
           },
         },
       ],
