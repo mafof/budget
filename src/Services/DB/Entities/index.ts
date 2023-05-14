d49530acefa38c5b56c64d692d8b8cb8988c5789
mafof <pavel.bykov@mafof.ru>
2023-05-14 12:04:22 +0300

    Добавил Entity

---
+++ src/Services/DB/Entities/index.ts
@@ -0,0 +1,13 @@
+import OperationList from "./OperationList";
+import ProductList from "./ProductList";
+import CostProduct from "./CostProduct";
+import WalletList from "./WalletList";
+import Settings from "./Settings";
+
+export {
+  OperationList,
+  ProductList,
+  CostProduct,
+  WalletList,
+  Settings
+}
